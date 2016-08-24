7d73e74630f287fd2ddeea88843ccc5b7c1ba9ad
Leonardo dos Santos <laugustogs@gmail.com>
2016-08-24 16:11:52 -0500

    Added estimation of the uncertainties underestimation factor log(f)

--- keppy/estimate.py
+++ keppy/estimate.py
@@ -52,7 +52,7 @@ class OrbitalParams(object):
         is necessary because different instruments have different offsets in
         the radial velocities. Default is 1.
     """
-    def __init__(self, t, rv, rv_err, guess, bounds_vz,
+    def __init__(self, t, rv, rv_err, guess, bounds_vz, bounds_logf,
                  bounds=((-4, 4), (-4, 4), (0, 10000), (0, 360),
                          (-4, -4.3E-5)), n_datasets=1):
 
@@ -67,22 +67,22 @@ class OrbitalParams(object):
             self.t = t
             self.rv = rv
             self.rv_err = rv_err
-            if len(guess) != 5+self.n_datasets:
+            if len(guess) != 5 + 2 * self.n_datasets:
                 raise ValueError('guess must have a length equal to 5 + '
-                                 'n_datasets')
+                                 '2 * n_datasets')
             else:
                 self.guess = guess
-            self.bounds = bounds + (bounds_vz,)
+            self.bounds = bounds + (bounds_vz,) + (bounds_logf,)
         else:
             self.t = t
             self.rv = rv
             self.rv_err = rv_err
-            if len(guess) != 5+self.n_datasets:
+            if len(guess) != 5 + 2 * self.n_datasets:
                 raise ValueError('guess must have a length equal to 5 + '
-                                 'n_datasets')
+                                 '2 * n_datasets')
             else:
                 self.guess = guess
-            self.bounds = bounds + bounds_vz
+            self.bounds = bounds + bounds_vz + bounds_logf
 
     # The likelihood function
     # noinspection PyTypeChecker
@@ -108,7 +108,10 @@ class OrbitalParams(object):
                                         t0=theta[2], w=theta[3], log_e=theta[4],
                                         vz=theta[5 + i])
             model = system.get_rvs(ts=self.t[i], nt=nt)
-            inv_sigma2 = 1. / self.rv_err[i] ** 2
+            inv_sigma2 = 1 / (self.rv_err[i] ** 2 + model ** 2 *
+                              10 ** (2 * theta[5 + self.n_datasets + i]))
+            # inv_sigma2 = 1. / (self.rv_err[i] ** 2 +
+            #                   theta[5 + self.n_datasets + i] ** 2)
             sum_like += np.sum((self.rv[i] - model) ** 2 * inv_sigma2 +
                                np.log(2. * np.pi / inv_sigma2))
         sum_like *= -0.5
@@ -195,9 +198,6 @@ class OrbitalParams(object):
         :param nsteps: int
             Number of burning-in steps
 
-        :param ncut: int
-            Number of steps to ignore in the beginning of the burning-in phase
-
         :param nthreads: int
             Number of threads in your machine
 
@@ -205,7 +205,7 @@ class OrbitalParams(object):
             emcee samples that can be used to make a triangle plot using the
             corner routine
         """
-        ndim = 5 + self.n_datasets
+        ndim = 5 + 2 * self.n_datasets
         pos = np.array([self.guess + 1e-2 * np.random.randn(ndim)
                         for i in range(nwalkers)])
 
@@ -267,8 +267,10 @@ if __name__ == '__main__':
         rv_d[i] -= rv_m[i]
 
     # We use the true values as the initial guess for the orbital parameters
+    # The last two values are for the estimates of logf, which is the log10(f),
+    # where f is the underestimating factor of the errorbars of each dataset
     _guess = [np.log10(k_true), np.log10(period_true), t0_true, w_true,
-              np.log10(e_true), 0.0, 0.0]
+              np.log10(e_true), 0.0, 0.0, 0.0, 0.0]
 
     print('\n-------------------------------------------------------------')
     print('Starting maximum likelihood estimation.')
@@ -276,17 +278,19 @@ if __name__ == '__main__':
 
     # We instantiate the class OrbitalParams with our data
     estim = OrbitalParams(t_d, rv_d, rv_derr, guess=_guess, n_datasets=2,
-                          bounds_vz=((-1, 1), (-1, 1)))
+                          bounds_vz=((-1, 1), (-1, 1)),
+                          bounds_logf=((-5, 3), (-5, 3)))
 
     # And run the estimation
-    params_ml = estim.ml_orbit(disp=True, maxiter=500)
+    params_ml = estim.ml_orbit(disp=True, maxiter=1000)
     print('Orbital parameters estimation took %.4f seconds.' %
           (time.time() - start_time))
     print('\nResults:')
     print('K = %.3f, T = %.2f, t0 = %.1f, w = %.1f, e = %.3f, vz0 = %.3f, '
-          'vz1 = %.3f' %
+          'vz1 = %.3f, f0 = %.3f, f1 = %.3f' %
           (10 ** params_ml[0], 10 ** params_ml[1], params_ml[2],
-           params_ml[3], 10 ** params_ml[4], params_ml[5], params_ml[6]))
+           params_ml[3], 10 ** params_ml[4], params_ml[5], params_ml[6],
+           10 ** params_ml[7], 10 ** params_ml[8]))
     print('\n"True" values:')
     print('K = %.3f, T = %.2f, t0 = %.1f, w = %.1f, e = %.3f, vz0 = %.3f, '
           'vz1 = %.3f' %
@@ -298,10 +302,10 @@ if __name__ == '__main__':
     print('Starting emcee estimation. It can take a few minutes.')
     start_time = time.time()
     _sampler = estim.emcee_orbit(nwalkers=20,
-                                 nsteps=1000,
+                                 nsteps=10000,
                                  nthreads=4)
-    ncut = 100
-    ndim = 7
+    ncut = 5000
+    ndim = 9
     _samples = samples = _sampler.chain[:, ncut:, :].reshape((-1, ndim))
     print('\nOrbital parameters estimation took %.4f seconds.' %
           (time.time() - start_time))
@@ -309,10 +313,11 @@ if __name__ == '__main__':
     print('Now creating the corner plot.')
     corner.corner(_samples,
                   labels=[r'$\log{K}$', r'$\log{T}$', r'$t_0$', r'$\omega$',
-                          r'$\log{e}$', r'$v_Z1$', r'$v_Z2$'],
+                          r'$\log{e}$', r'$v_{Z0}$', r'$v_{Z1}$',
+                          r'$\log{f_0}$', r'$\log{f_1}$'],
                   truths=[np.log10(k_true), np.log10(period_true), t0_true,
                           w_true, np.log10(e_true), vz[0]-rv_m[0],
-                          vz[1]-rv_m[1]])
+                          vz[1]-rv_m[1], 0.0, 0.0])
     plt.savefig('corner.png')
     plt.show()
 
@@ -320,11 +325,13 @@ if __name__ == '__main__':
     _samples[:, 0] = 10 ** _samples[:, 0]
     _samples[:, 1] = 10 ** _samples[:, 1]
     _samples[:, 4] = 10 ** _samples[:, 4]
+    _samples[:, 7] = 10 ** _samples[:, 7]
+    _samples[:, 8] = 10 ** _samples[:, 8]
 
     # Printing results
-    k_mcmc, period_mcmc, t0_mcmc, w_mcmc, e_mcmc, vz0_mcmc, vz1_mcmc = map(
-        lambda v: np.array([v[1], v[2] - v[1], v[1] - v[0]]),
-        zip(*np.percentile(_samples, [16, 50, 84], axis=0)))
+    k_mcmc, period_mcmc, t0_mcmc, w_mcmc, e_mcmc, vz0_mcmc, vz1_mcmc, f0_mcmc, \
+        f1_mcmc = map(lambda v: np.array([v[1], v[2] - v[1], v[1] - v[0]]),
+                      zip(*np.percentile(_samples, [16, 50, 84], axis=0)))
 
     print('\nResults:')
     print('K = %.3f + (+ %.3f, -%.3f)' % (k_mcmc[0], k_mcmc[1], k_mcmc[2]))
@@ -334,9 +341,11 @@ if __name__ == '__main__':
     print('w = %.1f + (+ %.1f, -%.1f)' % (w_mcmc[0], w_mcmc[1], w_mcmc[2]))
     print('e = %.3f + (+ %.3f, -%.3f)' % (e_mcmc[0], e_mcmc[1], e_mcmc[2]))
     print(
-        'vz = %.3f + (+ %.3f, -%.3f)' % (vz0_mcmc[0], vz0_mcmc[1], vz0_mcmc[2]))
+       'vz0 = %.3f + (+ %.3f, -%.3f)' % (vz0_mcmc[0], vz0_mcmc[1], vz0_mcmc[2]))
     print(
-        'vz = %.3f + (+ %.3f, -%.3f)' % (vz1_mcmc[0], vz1_mcmc[1], vz1_mcmc[2]))
+       'vz1 = %.3f + (+ %.3f, -%.3f)' % (vz1_mcmc[0], vz1_mcmc[1], vz1_mcmc[2]))
+    print('f0 = %.3f + (+ %.3f, -%.3f)' % (f0_mcmc[0], f0_mcmc[1], f0_mcmc[2]))
+    print('f1 = %.3f + (+ %.3f, -%.3f)' % (f1_mcmc[0], f1_mcmc[1], f1_mcmc[2]))
     print('\nFinished testing emcee estimation.')
     print('---------------------------------------------------------------')
     print('Plotting the results.')
@@ -360,7 +369,7 @@ if __name__ == '__main__':
     plt.plot(ts, rvs_mcmc, label='emcee')
 
     # Plotting various samples from MCMC
-    s_redux = _samples[:, 0:-2]
+    s_redux = _samples[:, 0:-4]
     for k, T, t0, w, e in s_redux[np.random.randint(len(s_redux), size=200)]:
         est = orbit.BinarySystem(log_k=np.log10(k),
                                  log_period=np.log10(T),
