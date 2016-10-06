b3614c771922872ac8d83b482f5cf7dd2a7be238
Leonardo dos Santos <laugustogs@gmail.com>
2016-10-06 15:28:06 -0500

    Better setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'description': 'Stars and radial velocity data analysis',
-    'author': 'Leonardo dos Santos',
-    'download_url': 'https://github.com/RogueAstro/keppy',
-    'author_email': 'leonardoags@usp.br',
-    'version': '0.1.dev',
-    'install_requires': ['numpy', 'scipy', 'emcee', 'matplotlib', 'corner'],
-    'packages': ['keppy'],
-    'name': 'keppy'
-}
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
 
-setup(**config)
+setup(
+    name="keppy",
+    version="0.2a",
+    author="Leonardo dos Santos",
+    author_email="ldsantos@uchicago.edu",
+    packages=["keppy"],
+    url="https://github.com/RogueAstro/MAROON-X_DRS",
+    license="MIT",
+    description="Data reduction software for the spectrograph MAROON-X",
+    install_requires=["numpy", "scipy", "emcee"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
