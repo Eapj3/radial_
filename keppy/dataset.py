4573e0536b94df2bf642c813eb9cea69ca849924
Leonardo dos Santos <laugustogs@gmail.com>
2016-11-10 18:03:29 -0600

    First commit of dataset module

---
+++ keppy/dataset.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+
+"""
+This module is used to manipulate radial velocities dataset.
+"""
+
+
+# The RV dataset class
+class RVDataSet(object):
+
+    def __init__(self):
+        pass
