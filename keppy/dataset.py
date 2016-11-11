75e6f460a2b5c67959ee85510ae5b64b2e67ec31
Leonardo dos Santos <laugustogs@gmail.com>
2016-11-10 21:39:28 -0600

    Added plot method and other metadata options

--- keppy/dataset.py
+++ keppy/dataset.py
@@ -6,6 +6,7 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 import astropy.table as t
 import astropy.units as u
+import matplotlib.pyplot as plt
 
 """
 This module is used to manage radial velocities datasets.
@@ -62,12 +63,21 @@ class RVDataSet(object):
         ``astropy.units``. If ``None``, uses km/s. Default is ``None``.
 
     instrument_name : ``str`` or ``None``, optional
-        Name of the instrument, which will be save in the metadata. Default is
+        Name of the instrument, which will be saved in the metadata. Default is
         ``None``.
+
+    target_name : ``str`` or ``None``, optional
+        Name of the observed target, which will be saved in the metadata.
+        Default is ``None``.
+
+    other_meta : ``dict`` or ``None``, optional
+        Other metadata to be saved in the table. If ``None``, no addition is
+        made. Default is ``None``.
     """
     def __init__(self, file, t_col=0, rv_col=1, rv_unc_col=2, skiprows=0,
                  delimiter=None, t_offset=None, rv_offset=None, t_unit=None,
-                 rv_unit=None, instrument_name=None):
+                 rv_unit=None, instrument_name=None, target_name=None,
+                 other_meta=None):
 
         # Setting up the default values
         # The time unit
@@ -80,6 +90,15 @@ class RVDataSet(object):
             self.rv_unit = u.km / u.s
         else:
             self.rv_unit = rv_unit
+        # Other stuff
+        if instrument_name is None:
+            self.instr_name = 'Unnamed instrument'
+        else:
+            self.instr_name = instrument_name
+        if target_name is None:
+            self.target_name = 'Unnamed target'
+        else:
+            self.target_name = target_name
 
         # Read the data from file
         self.t = np.loadtxt(file, usecols=(t_col,), skiprows=skiprows,
@@ -114,10 +133,22 @@ class RVDataSet(object):
         # Create an astropy table with the data
         self.table = t.Table([self.t, self.rv, self.rv_unc],
                              names=['Time', 'RV', 'RV sigma'],
-                             meta={'Instrument': instrument_name,
+                             meta={'Instrument': self.instr_name,
                                    'Time offset': self.t_offset,
-                                   'RV offset': self.rv_offset})
+                                   'RV offset': self.rv_offset,
+                                   'Target': self.target_name})
+
+        # Add the optional extra metadata
+        if isinstance(other_meta, dict):
+            self.table.meta.update(other_meta)
 
     # Plot the data
     def plot(self):
-        pass
+        plt.errorbar(self.t.value, self.rv.value, yerr=self.rv_unc.value,
+                     fmt='ko')
+        plt.title("{} observed on {}".format(self.table.meta['Target'],
+                                             self.table.meta['Instrument']))
+        plt.xlabel('Time ({})'.format(self.t.unit))
+        plt.ylabel('Radial velocities ({})'.format(self.rv.unit))
+        plt.show()
+
