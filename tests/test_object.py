ece20b8fb340305013380fc83bc25346641cb7ce
Leonardo dos Santos <leonardoags@usp.br>
2017-03-09 16:44:19 -0300

    Small improvements in test_object

--- tests/test_object.py
+++ tests/test_object.py
@@ -7,6 +7,7 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 from radial import object
 import astropy.units as u
+import matplotlib.pyplot as plt
 
 
 A = object.MainStar(mass=0.954 * u.solMass)
@@ -15,7 +16,18 @@ B = object.Companion(k=6.314 * u.km / u.s,
                      t_0=4904.5 * u.d,
                      omega=139.3 * u.deg,
                      ecc=0.343)
+b = object.Companion(k=0.5 * u.km / u.s,
+                     period_orb=700 * u.d,
+                     t_0=2000.5 * u.d,
+                     omega=180.3 * u.deg,
+                     ecc=0.543)
 
-HIP67620 = object.System(main_star=A, companions=[B], name='HIP67620')
+# time = (np.loadtxt('HIP67620_WF16.dat', usecols=(1,)) - 5E4) * u.d
+time = np.linspace(2000, 7000, 1000) * u.d
+HIP67620 = object.System(main_star=A, companion=[B, b], name='HIP67620',
+                         time=time)
 HIP67620.mass_func()
-print(HIP67620.companions[0].msini)
+
+HIP67620.compute_rv()
+HIP67620.plot_rv()
+plt.show()
